036d1a69f06b57106b5a2ff36caf99abff67d3fd
winston <winston@anonysec.dev>
2026-07-24 13:29:29 +0000

    Remove session artifacts: symlinks, bot/, scripts/, shared/, alembic migrations, authz module

--- docker-entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-# Start bot in background (using uv to manage dependencies)
-uv run -C bot python -m bot.main &
-
-# Run panel in foreground
-exec uv run python main.py
--- scripts/restart-ov.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd /root/workspace
-docker compose up -d
-sleep 5
-docker ps --format 'table {{.Names}}\t{{.Health}}'
